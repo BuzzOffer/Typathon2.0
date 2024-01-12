dd92da53ba855814de820c5dab87e535aae6c5bb
Simon <hawyee@gmail.com>
2024-01-12 16:59:04 +0800

    added back button in leaderboards page

--- src/main/java/com/example/typathon2/LeaderboardController.java
+++ src/main/java/com/example/typathon2/LeaderboardController.java
@@ -6,9 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.IOException;
@@ -19,12 +17,13 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
-import javafx.scene.control.Label;
+
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class LeaderboardController {
-
+    @FXML
+    private Button backButton;
     @FXML
     private TableView<LeaderboardEntry> leaderboardTable;
 
@@ -178,6 +177,19 @@ public class LeaderboardController {
             prof.loadProfileData();
             Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
             Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            // Handle exception
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    public void goBack(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(scene);
             stage.show();
         } catch (IOException e) {
